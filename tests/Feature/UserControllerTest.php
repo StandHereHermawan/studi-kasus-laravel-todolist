2f29d92663036f7795cd5f8dc3bf75c0c29a159e
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-19 23:06:59 +0700

    Membuat login Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -18,4 +18,40 @@ class UserControllerTest extends TestCase
             ->assertSeeText("Sign")
             ->assertSeeText("In");
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            "user" => "andrew",
+            "password" => "rasis@born"
+        ])->assertRedirect("/")
+            ->assertSessionHas("user", "andrew");
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post('/login', [])
+            ->assertSeeText("Username or password is required")
+            ->assertSeeText("Login")
+            ->assertSeeText("User")
+            ->assertSeeText("Password")
+            ->assertSeeText("Programmer")
+            ->assertSeeText("Sign")
+            ->assertSeeText("In");
+    }
+
+    public function testLoginWrongUsernameOrPassword()
+    {
+        $this->post('/login', [
+            "user" => "oke",
+            "password" => "oke"
+        ])
+            ->assertSeeText("Username or password is wrong")
+            ->assertSeeText("Login")
+            ->assertSeeText("User")
+            ->assertSeeText("Password")
+            ->assertSeeText("Programmer")
+            ->assertSeeText("Sign")
+            ->assertSeeText("In");
+    }
 }
