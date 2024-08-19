190361a3108eb920bd8c1b8f12390f0423128379
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-19 22:23:25 +0700

    Membuat Login Page

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -8,5 +8,14 @@ use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
-    public function test() {}
+    public function testViewLoginPage()
+    {
+        $this->get('/login')
+            ->assertSeeText("Login")
+            ->assertSeeText("User")
+            ->assertSeeText("Password")
+            ->assertSeeText("Programmer")
+            ->assertSeeText("Sign")
+            ->assertSeeText("In");
+    }
 }
