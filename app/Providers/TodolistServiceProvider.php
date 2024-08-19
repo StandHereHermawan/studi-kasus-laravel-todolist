813066331b597bc6c7198b31ccabfe68f79e2874
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-20 01:52:39 +0700

    Membuat TodolistService

---
+++ app/Providers/TodolistServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\TodolistService;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class TodolistServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public array $singletons = [
+        \App\Services\TodolistService::class => \App\Services\Impl\TodolistServiceImpl::class
+    ];
+
+    public function provides(): array
+    {
+        return [\App\Services\TodolistService::class];
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
